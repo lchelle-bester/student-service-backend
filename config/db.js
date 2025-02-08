b5fc69625a47d07c8aa222589388a2aa903c3231
lchelle-bester <lchelle.best@gmail.com>
2025-02-08 13:22:28 +0200

    Fixed database connection configuration

--- config/db.js
+++ config/db.js
@@ -1,16 +1,23 @@
-const { Pool } = require('pg');
-require('dotenv').config();
-const { Pool } = require('pg');
+const pg = require('pg');
 require('dotenv').config();
 
-const pool = new Pool({
+const pool = new pg.Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
     }
 });
 
+// Test the connection
+pool.on('connect', () => {
+    console.log('Database connected successfully');
+});
+
+pool.on('error', (err) => {
+    console.error('Unexpected database error:', err);
+});
+
 module.exports = {
     query: (text, params) => pool.query(text, params),
-    pool
+    pool: pool
 };
